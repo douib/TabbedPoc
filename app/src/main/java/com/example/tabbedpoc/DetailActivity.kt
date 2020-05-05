fc753a41eaf61e97d2b7b90183262f46c7a5de52
douib <mounir.douib@easybroadcast.fr>
2020-05-05 15:40:09 +0200

    first commit

---
+++ app/src/main/java/com/example/tabbedpoc/DetailActivity.kt
@@ -0,0 +1,23 @@
+package com.example.tabbedpoc
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_detail.*
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
